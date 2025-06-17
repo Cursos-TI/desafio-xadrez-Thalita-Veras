1a8007c675e4a302e19be0aee026c05bce0970a9
Thalita  Veras <thalitaveras31@gmail.com>
2025-06-17 13:59:09 +0000

    Atualização Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,74 @@
 #include <stdio.h>
-#include <windows.h>
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+// Nível Mestre - Funções Recursivas e Loops Aninhados
+// Sugestão: Substitua as movimentações das peças por funções recursivas.
+// Exemplo: Crie uma função recursiva para o movimento do Bispo.
+
+void conversaoDirecao (int escolha) {
+    if(escolha == 1) {
+        printf("Cima\n");
+    } else if (escolha == 2) {
+        printf("Baixo\n");
+    } else if (escolha == 3) {
+        printf("Direita\n");
+    }  else if (escolha == 4) {
+        printf("Esquerda\n");
+    }
+}
+
+void movimentoLoop(int escolha, int n) {
+    if(n > 0) {
+        conversaoDirecao(escolha);
+        movimentoLoop(escolha, n - 1);
+    }
+}
+
+void movimentoBispo(int escolha1, int escolha2, int casas) {
+    if(casas == 0) return;
+
+    for (int i = 0; i < 1; i++) {
+        if (escolha1 == 1) printf("Cima\n");
+        else if (escolha1 == 2) printf("Baixo\n");
+    
+
+        for(int j = 0; j < 1; j++) {
+            if (escolha2 == 1) printf("Direita\n");
+            else if (escolha2 == 2) printf("Esquerda\n");
+        }
+    }
+
+    movimentoBispo(escolha1, escolha2, casas - 1);
+}
+
+void movimentoCavalo() {
+    for (int i = 0; i < 2; i++) {
+        printf("Cima\n");
+        if(i == 1) break; // Para após duas casas
+    }
+
+    for (int j = 0; j < 2; j++) {
+        if(j == 0) {
+            printf("Direita\n");
+            continue;
+        }
+    }
+}
+
+void tempo() {
+    for (int i = 0; i < 5; i++) {
+        printf(".\n");
+        for (int j = 0; j < 10; j++) {
+        }
+    }
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int i;
     int opcao, escolha;
 
     do {
@@ -23,64 +83,41 @@ int main() {
         
         printf("\n");
 
-        for (int i = 0; i < 5; i++) {
-            printf(".\n");
-            Sleep(1);
-        }
+        tempo();
 
         printf("\n");
-
+        int escolha2;
         switch(opcao) {
             case 1:
                 do {
                     printf("1. Cima\n");
                     printf("2. Baixo\n");
-                    printf("3. Direita\n");
-                    printf("4. Esquerda\n");
-                    printf("5. Voltar\n");
-                    printf("Escolha uma opcao: ");
+                    printf("3. Voltar\n");
+                    printf("Escolha a direcao vertical: ");
                     scanf("%d", &escolha);
+                    
+                    if(escolha == 3) break;
 
-                    i = 1;
+                    printf("1. Direita\n");
+                    printf("2. Esquerda\n");
+                    printf("3. Voltar\n");
+                    printf("Escolha a direcao horizontal: ");
+                    scanf("%d", &escolha2);
 
                     switch (escolha) {
-                    case 1:
-                        printf("Cima.\n");
-                        while (i <= 5) {
+                        case 1:
+                            movimentoBispo(escolha, escolha2, 5);
+                            break;
+                        case 2:
                             printf("Diagonal\n");
-                            i++;
-                        }
-                        break;
-                    case 2:
-                        printf("Baixo.\n");
-                        while (i <= 5) {
-                            printf("Diagonal\n");
-                            i++;
-                        }
-                        break;
-                    case 3:
-                        printf("Direita.\n");
-                        while (i <= 5) {
-                            printf("Diagonal\n");
-                            i++;
-                        }
-                        break;
-                    case 4:
-                        printf("Esquerda.\n");
-                        while (i <= 5) {
-                            printf("Diagonal\n");
-                            i++;
-                        }
-                        break;
-                    default:
-                        printf("Voltando...\n");
-                        for (int i = 0; i < 5; i++) {
-                            printf(".\n");
-                            Sleep(1);
-                            }
-                        break;
+                            movimentoBispo(escolha, escolha2, 5);
+                            break;
+                        default:
+                            printf("Voltando...\n");
+                            tempo();
+                            break;
                     }
-                } while (escolha != 5);   
+                } while (escolha != 3);   
                 break;
             case 2:
                 // Implementação de Movimentação da Torre
@@ -96,31 +133,20 @@ int main() {
                 
                     switch (escolha) {
                         case 1:
-                            for (int j = 1; j <= 5; j++) {
-                                printf("Cima\n");
-                            }
+                            movimentoLoop(escolha, 5);
                             break;
                         case 2:
-                            for (int j = 1; j <= 5; j++) {
-                                printf("Baixo\n");
-                            }
+                            movimentoLoop(escolha, 5);
                             break;
                         case 3:
-                            for (int j = 1; j <= 5; j++) {
-                                printf("Direita\n");
-                            }
+                            movimentoLoop(escolha, 5);
                             break;
                         case 4:
-                            for (int j = 1; j <= 5; j++) {
-                                printf("Esquerda\n");
-                            }
+                            movimentoLoop(escolha, 5);
                             break;
                         default:
                             printf("Voltando...\n");
-                            for (int i = 0; i < 5; i++) {
-                                printf(".\n");
-                                Sleep(1);
-                            }
+                            tempo();
                             break;
                     }
                 } while (escolha != 5); 
@@ -137,36 +163,23 @@ int main() {
                     printf("5. Voltar\n");
                     printf("Escolha uma opcao: ");
                     scanf("%d", &escolha);
-                    
-                    int k = 1;
 
                     switch (escolha) {
                         case 1:
-                            do {
-                                printf("Cima\n");
-                                k++;
-                            } while (k <= 8);
+                            movimentoLoop(escolha, 8);
                             break;
                         case 2:
-                            do {
-                                printf("Baixo\n");
-                                k++;
-                            } while (k <= 8);
+                            movimentoLoop(escolha, 8);
                             break;
                         case 3:
-                            do {
-                                printf("Direita\n");
-                                k++;
-                            } while (k <= 8);
+                            movimentoLoop(escolha, 8);
                             break;
                         case 4:
-                            do {
-                                printf("Esquerda\n");
-                                k++;
-                            } while (k <= 8);
+                            movimentoLoop(escolha, 8);
                             break;
                         default:
                             printf("Voltando...\n");
+                            tempo();
                             break;
                     }
                 } while (escolha != 5); 
@@ -174,62 +187,15 @@ int main() {
                 break;
             case 4:
                 // Nível Aventureiro - Movimentação do Cavalo
-                do {
-                    printf("1. Direita\n");
-                    printf("2. Esquerda\n");
-                    printf("3. Voltar\n");
-                    printf("Escolha uma opcao: ");
-                    scanf("%d", &escolha);
-
-                    int movimentoCompleto = 1;
-
-                    switch (escolha) {
-                        case 1:
-                            while (movimentoCompleto--) {
-                                for (int i = 0; i < 2; i++) {
-                                    printf("Cima\n");
-                                }
-                                printf("Direita\n");
-                            }
-                            break;
-                        case 2:
-                            while (movimentoCompleto--) {
-                                for (int i = 0; i < 2; i++) {
-                                    printf("Cima\n");
-                                }
-                                printf("Esquerda\n");
-                            }
-                            break;
-                        default:
-                            printf("Voltando...\n");
-                            break;
-                    }
-                } while (escolha != 3); 
-                
-
-               
+                movimentoCavalo();
+                printf("Movimento do Cavalo realizado.\n");
                 break;
             default:
-                printf("Saindo...");
-                for (int i = 0; i < 5; i++) {
-                    printf(".\n");
-                    Sleep(1);
-                }
+                printf("Saindo...\n");
+                printf("Obrigado por jogar!\n");
                 break;
         }
     } while (opcao != 5);
-    
-
-    
-    
-    
-    
-    
-    
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
